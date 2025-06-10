c045aa1f889b1491462e474c8702b2284b921c4e
Marvicss <marconivictor15@gmail.com>
2025-06-10 17:32:14 -0300

    adde BottomMenu and adjust Component

--- components/BottomMenu.tsx
+++ components/BottomMenu.tsx
@@ -8,19 +8,19 @@ const BottomMenu: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.push('./home')}>
+      <TouchableOpacity onPress={() => router.push('/home')}>
         <Feather name="home" size={24} color="black" />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => router.push('/finance')}>
         <Feather name="file-text" size={24} color="black" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push('./notice')}>
+      <TouchableOpacity onPress={() => router.push('/notice')}>
         <Feather name="volume-2" size={24} color="black" />
       </TouchableOpacity>
       <TouchableOpacity onPress={() => router.push('/plan')}>
         <Feather name="edit-2" size={24} color="black" />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push('/calendar')}>
+      <TouchableOpacity onPress={() => router.push('/reservas/morador')}>
         <Feather name="calendar" size={24} color="black" />
       </TouchableOpacity>
     </View>
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     alignItems: 'center',
     paddingVertical: 12,
-    paddingBottom:20,
+    paddingBottom: 20,
     borderTopLeftRadius: 12,
     borderTopRightRadius: 12,
   },
