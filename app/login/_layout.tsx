4cf4a6e1aa846759327821ae186db67db108f40b
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2025-10-05 12:45:37 +0000

    feat: cria tela de login para sindico com rota

---
+++ app/login/_layout.tsx
@@ -0,0 +1,12 @@
+// app/login/_layout.tsx
+import { Stack } from 'expo-router';
+
+export default function LoginLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
